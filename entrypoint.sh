70987b476fdd6d49848a3aa608ef84724b38dca8
stodr <78125061+s0meth1ng2dr1nk@users.noreply.github.com>
2023-06-10 20:47:10 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-tmux new-session -d bash /tmod/tmodloader/start-tModLoaderServer.sh -nosteam -config /opt/tmod/tModLoader/server.config
-#-savedirectory /opt/tmod 
+tmux new-session -d bash /tmod/tmodloader/start-tModLoaderServer.sh -nosteam -config /opt/tmod/tModLoader/server.config -savedirectory /opt/tmod 
 while true; do
     sleep 300 && \
     tmux send-keys "save" Enter
